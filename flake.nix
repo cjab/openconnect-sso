22971b573f919c393e0b07125596d532877dd99a
Chad Jablonski <chad@jablonski.xyz>
2024-07-15 21:13:35 -0400

    Update poetry2nix flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    flake-utils.url = github:numtide/flake-utils;
+    flake-utils.url = "github:numtide/flake-utils";
 
     poetry2nix = {
-      url = github:nix-community/poetry2nix;
+      url = "github:nix-community/poetry2nix";
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nix-github-actions.follows = "nix-github-actions";
@@ -15,39 +15,40 @@
     # Unused but allows downstream to override versions and avoids duplicates
 
     nix-github-actions = {
-      url = github:nix-community/nix-github-actions;
+      url = "github:nix-community/nix-github-actions";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    systems.url = github:nix-systems/default;
+    systems.url = "github:nix-systems/default";
 
     treefmt-nix = {
-      url = github:numtide/treefmt-nix;
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: (inputs.flake-utils.lib.eachDefaultSystem (
-    system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+  outputs = { self, nixpkgs, ... }@inputs: (inputs.flake-utils.lib.eachDefaultSystem
+    (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
 
-      openconnect-pkgs = import ./nix {
-        inherit pkgs poetry2nix;
-        sources = null; # make sure we don't mix flakes and Niv
-      };
-    in
-    {
-      packages = rec {
-        inherit (openconnect-pkgs) openconnect-sso;
+        openconnect-pkgs = import ./nix {
+          inherit pkgs poetry2nix;
+          sources = null; # make sure we don't mix flakes and Niv
+        };
+      in
+      {
+        packages = rec {
+          inherit (openconnect-pkgs) openconnect-sso;
 
-        default = openconnect-sso;
-      };
+          default = openconnect-sso;
+        };
 
-      devShells.default = openconnect-pkgs.shell;
-    }
-  ) // {
+        devShells.default = openconnect-pkgs.shell;
+      }
+    ) // {
     overlays = rec {
       default = openconnect-sso;
 
